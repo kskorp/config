18f9900b0dd0d6e450384c978ee1111f0d225bea
Andrzej Sawoniewicz <a.sawoniewicz@kainos.com>
2013-10-03 13:31:04 +0200

    add form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,24 +2,50 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import models.Configuration._
-import models.ConfigurationDAO
+import models._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
+	val dao = new ConfigDAO
+  
+	val editConfigForm = Form(
+		"config" ->
+			mapping(
+				"ref" -> longNumber,
+				"name" -> text,
+				"timestamp" -> longNumber
+			)(Config.apply)(Config.unapply)
+	)
   
 	def index = Action {
 		Redirect(routes.Application.list)
 	}
 	
 	def list = Action {
-		val dao = new ConfigurationDAO
-		val configurations = dao.findAll;
-		Ok(views.html.list(configurations))
+		val configs = dao.findAll;
+		Ok(views.html.list(configs))
 	}
 	
-	def create() = TODO
+	def create() = Action {
+		val config = Config(0, "", 0)
+		Ok(views.html.edit(config, editConfigForm, false))
+	}
 	
-	def edit(ref: Long) = TODO
+	def edit(ref: Long) = Action {
+		dao.findByRef(ref).map { config =>
+			Ok(views.html.edit(config, editConfigForm, false))
+		}.getOrElse(NotFound)
+	}
+	
+	def editpost(ref: Long) = Action { implicit request =>
+		dao.findByRef(ref).map { config =>
+			editConfigForm.bindFromRequest.fold(
+				errors => BadRequest(views.html.edit(config, errors, true)),
+				config => Ok(views.html.edit(config, editConfigForm, true))
+			)
+		}.getOrElse(NotFound)
+	}
 	
 	def delete(ref: Long) = TODO
   
--- app/models/Configuration.scala
+++ app/models/Configuration.scala
@@ -1,22 +1,22 @@
 package models
 
-case class Configuration(val ref: Long, val timestamp: Long, val name: String)
+case class Config(val ref: Long, val name: String, val timestamp: Long)
 
-trait ConfigurationDAOTrait {
-	def findAll: List[Configuration]
+trait ConfigDAOTrait {
+	def findAll: List[Config]
 	
-	def findByRef(ref: Long): Option[Configuration]
+	def findByRef(ref: Long): Option[Config]
 }
 
-class ConfigurationDAO extends ConfigurationDAOTrait {
-	val configurations = Set(
-	    Configuration(1, 1380728153, "Conf1"),
-	    Configuration(2, 1380693842, "Conf2"),
-	    Configuration(3, 1380684275, "Conf3")
+class ConfigDAO extends ConfigDAOTrait {
+	val configs = Set(
+	    Config(1, "Conf1", 1380728153),
+	    Config(2, "Conf2", 1380693842),
+	    Config(3, "Conf3", 1380684275)
 	)
 	
-	def findAll = this.configurations.toList.sortBy(_.ref)
+	def findAll = this.configs.toList.sortBy(_.ref)
 	
-	def findByRef(ref: Long) = this.configurations.find(_.ref == ref)
+	def findByRef(ref: Long) = this.configs.find(_.ref == ref)
 }
 
