6865fce8f93740229aaa929bc5388f787cc1cb80
Andrzej Sawoniewicz <a.sawoniewicz@kainos.com>
2013-10-06 23:33:09 +0200

    test db conn

--- app/models/Configuration.scala
+++ app/models/Configuration.scala
@@ -1,5 +1,10 @@
 package models
 
+import scala.slick.session.Database
+import Database.threadLocalSession
+import scala.slick.jdbc.{GetResult, StaticQuery => Q}
+import Q.interpolation
+
 case class Config(ref: Option[Long] = None, name: String = "", timestamp: Option[Long] = None)
 
 trait ConfigDAOTrait {
@@ -20,5 +25,15 @@ class ConfigDAO extends ConfigDAOTrait {
 	def findByRef(ref: Long) = this.configs.find(_.ref.get == ref)
 	
 	def deleteByRef(ref: Long) = {}
+	
+	def testrun = {
+		implicit val getConfigResult = GetResult(r => Config(r.<<, r.<<, r.<<))
+	  
+		Database.forURL("jdbc:postgresql://localhost/config?user=smartdb&password=TowarWydano01!@&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", driver = "org.postgresql.Driver") withSession {
+			Q.queryNA[Config]("select reference, name, extract(epoch from timestamp)::int as timestamp from a.configuration") foreach { c =>
+				println("  " + c.ref + "\t" + c.name + "\t" + c.timestamp)
+			}
+		}
+	}
 }
 
